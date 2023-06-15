075e009361dc1438eabefbf9e66cdc60dbc2a8b3
pan <79103252@qq.com>
2023-06-15 18:53:54 +0800

    script modify

--- run_scaennr.sh
+++ run_scaennr.sh
@@ -7,15 +7,18 @@ loc="$(cd "$(dirname "$BASH_SOURCE")" && pwd)"
 logs_dir="$loc/logs"
 mkdir -p "$logs_dir"
 
+# 定义容器名称
+container_name="ether_scanner"
+
 # 检查镜像是否存在
 if docker image inspect ether_scanner:1.0 >/dev/null 2>&1; then
     # 镜像已存在，直接运行容器
-    docker run -d -v "$logs_dir":/app/logs ether_scanner:1.0
+    docker run -d -v "$logs_dir":/app/logs --name "$container_name" ether_scanner:1.0
 else
     # 镜像不存在，先构建镜像再运行容器
-    docker build -t ether_scanner:1.0 "$loc" && docker run -d -v "$logs_dir":/app/logs ether_scanner:1.0
+    docker build -t ether_scanner:1.0 "$loc" && docker run -d -v "$logs_dir":/app/logs --name "$container_name" ether_scanner:1.0
 fi
 
 docker ps
 
-echo "ethers_scanner Startup complete..."
+echo "ether_scanner Startup complete..."
